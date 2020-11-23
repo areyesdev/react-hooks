7f953fd44e74b7a6961aebf66e1f0c96a03161a2
Andres Reyes <ar97yf@gmail.com>
2020-11-23 12:53:29 -0500

    [ADD] useContext

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Characters from "./components/Characters";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Characters />
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
